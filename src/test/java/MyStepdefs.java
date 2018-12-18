51367ec0a3e92523ed6789dc1cdc74387748ef39
alexandru-dan.bratfalean <alexandru-dan.bratfalean@microfocus.com>
2018-12-18 16:28:04 +0200

    Revert "cleaningTests"
    This reverts commit dae1c09

--- src/test/java/MyStepdefs.java
+++ src/test/java/MyStepdefs.java
@@ -9,63 +9,128 @@ import static junit.framework.TestCase.assertTrue;
 
 public class MyStepdefs {
 
+    @Given("^I'm in tablets search results$")
+    public void iMInTabletsSearchResults() throws Throwable {
+
+    }
+
+    @When("^filter by price lower than (\\d+)$")
+    public void filterByPriceLowerThan(int arg0) throws Throwable {
+       
+    }
+
+    @Then("^Only tablets with lower price will be presented$")
+    public void onlyTabletsWithLowerPriceWillBePresented() throws Throwable {
+
+    }
+
+    @Then("^Only tablets with lower price will be presented Only tablets with lower price will be presented Only tablets with lower price will be presented$")
+    public void onlyTabletsWithLowerPriceWillBePresentedOnlyTabletsWithLowerPriceWillBePresentedOnlyTabletsWithLowerPriceWillBePresented() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+
+    }
+
+    @Given("^I am in Octane$")
+    public void iAmInOctane() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+
+    }
+
+    @When("^Clicking the New Feature$")
+    public void clickingTheNewFeature() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+
+    }
+
+    @Then("^Feature dialog is open$")
+    public void featureDialogIsOpen() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+
+    }
+
+    @Given("^I am in ALM Octane$")
+    public void iAmInALMOctane() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+
+    }
+
     @Given("^ceva$")
     public void ceva() throws Throwable {
         // Write code here that turns the phrase above into concrete actions
-        Assert.assertTrue(false);
+
     }
 
     @When("^altceva$")
     public void altceva() throws Throwable {
         // Write code here that turns the phrase above into concrete actions
-        Assert.assertTrue(false);
+
     }
 
     @Then("^e ok$")
     public void eOk() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        Assert.assertTrue(false);
-    }
-
-    @Given("^Dan given$")
-    public void danGiven() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
         Assert.assertTrue(true);
-    }
-
-    @When("^Dan when$")
-    public void danWhen() throws Throwable {
         // Write code here that turns the phrase above into concrete actions
-        Assert.assertTrue(true);
-    }
 
-    @Then("^Dan then$")
-    public void danThen() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        Assert.assertTrue(true);
     }
 
     @Given("^smth$")
     public void smth() throws Throwable {
+        Assert.assertTrue(false);
         // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+
     }
 
     @When("^smth else$")
     public void smthElse() throws Throwable {
+        Assert.assertTrue(false);
         // Write code here that turns the phrase above into concrete actions
-        Assert.assertTrue(true);
+
     }
 
     @Then("^everything is ok$")
     public void everythingIsOk() throws Throwable {
         // Write code here that turns the phrase above into concrete actions
-        Assert.assertTrue(true);
+
     }
 
-    @Given("^I'm in tablets search results$")
-    public void iMInTabletsSearchResults() throws Throwable {
+    @Given("^Dan given$")
+    public void danGiven() throws Throwable {
         // Write code here that turns the phrase above into concrete actions
-        Assert.assertTrue(true);
+
     }
+
+    @When("^Dan when$")
+    public void danWhen() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+
+    }
+
+    @Then("^Dan then$")
+    public void danThen() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+
+    }
+
+
+//
+//    @Given("^a calculateCommercialMargin request$")
+//    public void aCalculateCommercialMarginRequest() throws Throwable {
+//        // Write code here that turns the phrase above into concrete actions
+//    }
+//
+//    @When("^a valid request is send$")
+//    public void aValidRequestIsSend() throws Throwable {
+//        // Write code here that turns the phrase above into concrete actions
+//    }
+//
+//    @Then("^a response with no error is expected$")
+//    public void aResponseWithNoErrorIsExpected() throws Throwable {
+//        // Write code here that turns the phrase above into concrete actions
+//    }
+//
+//    @And("^the loanProductType,loanAmount,loanAmountCurrency,repaymentType,repaymentMaturity,interestPeriodIndicator,interestPeriodicity,capitalPeriodicity,purposeCode,purposeCodeType,interestRateCalculationBasisNumerator,interestRateCalculationBasisDenominator,drawDownStartDate,loanLifecycleStatus,yearlyRealProductMargin,yearlyRealIndividualMargin,yearlyRealTheoreticalGlobalMargin,yearlyRealApplicableGlobalMargin,selectedPriceFlag,customerExternalId,operationStatus,code,message are not null$")
+//    public void the_loanProductType_loanAmount_loanAmountCurrency_repaymentType_repaymentMaturity_interestPeriodIndicator_interestPeriodicity_capitalPeriodicity_purposeCode_purposeCodeType_interestRateCalculationBasisNumerator_interestRateCalculationBasisDenominator_drawDownStartDate_loanLifecycleStatus_yearlyRealProductMargin_yearlyRealIndividualMargin_yearlyRealTheoreticalGlobalMargin_yearlyRealApplicableGlobalMargin_selectedPriceFlag_customerExternalId_operationStatus_code_message_are_not_null() throws Throwable {
+//        // Write code here that turns the phrase above into concrete actions
+//    }
+
 }
