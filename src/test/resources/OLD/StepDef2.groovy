d9ad1ffebdf6940f6ca2ac9fd8832f780722a16f
alexandru-dan.bratfalean <alexandru-dan.bratfalean@microfocus.com>
2018-12-18 16:09:22 +0200

    deletedTest

--- src/test/resources/OLD/StepDef2.groovy
+++ src/test/resources/OLD/StepDef2.groovy
@@ -8,5 +8,41 @@ this.metaClass.mixin(cucumber.api.groovy.EN)
 
 Given(~/^I am in the homepage$/) { ->
     // Write code here that turns the phrase above into concrete actions
-    throw new PendingException()
+
+}
+Given(~/^I'm in the Online Shopping homepage$/) { ->
+    // Write code here that turns the phrase above into concrete actions
+
+}
+When(~/^Searching for a tablet$/) { ->
+    // Write code here that turns the phrase above into concrete actions
+
+}
+Then(~/^Tablet results are displayed$/) { ->
+    // Write code here that turns the phrase above into concrete actions
+
+}
+Given(~/^I've got a search results list of tablets$/) { ->
+    // Write code here that turns the phrase above into concrete actions
+
+}
+When(~/^I'm picking one tablet from the list$/) { ->
+    // Write code here that turns the phrase above into concrete actions
+
+}
+Then(~/^the tablet details are displayed$/) { ->
+    // Write code here that turns the phrase above into concrete actions
+    t
+}
+Given(~/^I'm in Item Details page for a tablet$/) { ->
+    // Write code here that turns the phrase above into concrete actions
+
+}
+When(~/^I click the 'Add to Cart' button$/) { ->
+    // Write code here that turns the phrase above into concrete actions
+
+}
+Then(~/^the tablet is added to my cart$/) { ->
+    // Write code here that turns the phrase above into concrete actions
+
 }
