bff6465bf4254f8808de3c048b27e6d67da8a825
alexandru-dan.bratfalean <alexandru-dan.bratfalean@microfocus.com>
2018-12-27 11:10:33 +0200

    GTrefactor

--- src/test/java/MyStepdefs.java
+++ src/test/java/MyStepdefs.java
@@ -21,122 +21,19 @@ public class MyStepdefs {
 
     @Then("^Only tablets with lower price will be presented$")
     public void onlyTabletsWithLowerPriceWillBePresented() throws Throwable {
+        Assert.assertTrue(false);
 
     }
 
     @Then("^Only tablets with lower price will be presented Only tablets with lower price will be presented Only tablets with lower price will be presented$")
     public void onlyTabletsWithLowerPriceWillBePresentedOnlyTabletsWithLowerPriceWillBePresentedOnlyTabletsWithLowerPriceWillBePresented() throws Throwable {
         // Write code here that turns the phrase above into concrete actions
-
     }
 
-    @Given("^I am in Octane$")
-    public void iAmInOctane() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-
-    }
-
-    @When("^Clicking the New Feature$")
-    public void clickingTheNewFeature() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-
-    }
-
-    @Then("^Feature dialog is open$")
-    public void featureDialogIsOpen() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-
-    }
-
-    @Given("^I am in ALM Octane$")
-    public void iAmInALMOctane() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-
-    }
-
-    @Given("^ceva$")
-    public void ceva() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-
-    }
-
-    @When("^altceva$")
-    public void altceva() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-
-    }
-
-    @Then("^e ok$")
-    public void eOk() throws Throwable {
-        Assert.assertTrue(true);
-        // Write code here that turns the phrase above into concrete actions
-
-    }
-
-    @Given("^smth$")
-    public void smth() throws Throwable {
-        Assert.assertTrue(false);
-        // Write code here that turns the phrase above into concrete actions
-
-    }
-
-    @When("^smth else$")
-    public void smthElse() throws Throwable {
-        Assert.assertTrue(false);
-        // Write code here that turns the phrase above into concrete actions
-
-    }
-
-    @Then("^everything is ok$")
-    public void everythingIsOk() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-
-    }
-
-    @Given("^Dan given$")
-    public void danGiven() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-
-    }
-
-    @When("^Dan when$")
-    public void danWhen() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-
-    }
-
-    @Then("^Dan then$")
-    public void danThen() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-
-    }
 
     @Then("^I should be logged in$")
     public void iShouldBeLoggedIn() throws Throwable {
         // Write code here that turns the phrase above into concrete actions
         Assert.assertTrue(false);
     }
-
-
-//
-//    @Given("^a calculateCommercialMargin request$")
-//    public void aCalculateCommercialMarginRequest() throws Throwable {
-//        // Write code here that turns the phrase above into concrete actions
-//    }
-//
-//    @When("^a valid request is send$")
-//    public void aValidRequestIsSend() throws Throwable {
-//        // Write code here that turns the phrase above into concrete actions
-//    }
-//
-//    @Then("^a response with no error is expected$")
-//    public void aResponseWithNoErrorIsExpected() throws Throwable {
-//        // Write code here that turns the phrase above into concrete actions
-//    }
-//
-//    @And("^the loanProductType,loanAmount,loanAmountCurrency,repaymentType,repaymentMaturity,interestPeriodIndicator,interestPeriodicity,capitalPeriodicity,purposeCode,purposeCodeType,interestRateCalculationBasisNumerator,interestRateCalculationBasisDenominator,drawDownStartDate,loanLifecycleStatus,yearlyRealProductMargin,yearlyRealIndividualMargin,yearlyRealTheoreticalGlobalMargin,yearlyRealApplicableGlobalMargin,selectedPriceFlag,customerExternalId,operationStatus,code,message are not null$")
-//    public void the_loanProductType_loanAmount_loanAmountCurrency_repaymentType_repaymentMaturity_interestPeriodIndicator_interestPeriodicity_capitalPeriodicity_purposeCode_purposeCodeType_interestRateCalculationBasisNumerator_interestRateCalculationBasisDenominator_drawDownStartDate_loanLifecycleStatus_yearlyRealProductMargin_yearlyRealIndividualMargin_yearlyRealTheoreticalGlobalMargin_yearlyRealApplicableGlobalMargin_selectedPriceFlag_customerExternalId_operationStatus_code_message_are_not_null() throws Throwable {
-//        // Write code here that turns the phrase above into concrete actions
-//    }
-
 }
