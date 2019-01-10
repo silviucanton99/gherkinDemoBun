f9547b4a21b6baa27a1d84544e5a18b4fa3c2fab
alexandru-dan.bratfalean <alexandru-dan.bratfalean@microfocus.com>
2019-01-10 14:20:19 +0200

    GT4TestsAllPass

--- src/test/java/MyStepdefs.java
+++ src/test/java/MyStepdefs.java
@@ -20,78 +20,70 @@ public class MyStepdefs {
 
     @Then("^Only tablets with lower price will be presented$")
     public void onlyTabletsWithLowerPriceWillBePresented() throws Throwable {
-        Assert.assertTrue(false);
+//        Assert.assertTrue(false);
 
     }
 
     @Then("^Only tablets with lower price will be presented Only tablets with lower price will be presented Only tablets with lower price will be presented$")
     public void onlyTabletsWithLowerPriceWillBePresentedOnlyTabletsWithLowerPriceWillBePresentedOnlyTabletsWithLowerPriceWillBePresented() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
+
     }
 
 
     @Given("^some no failed given$")
     public void someNoFailedGiven() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
 
     }
 
     @When("^I submit username and password$")
     public void iSubmitUsernameAndPassword() throws Throwable {
-        assertEquals(6, Calcs.sum(1, 5));
+//        assertEquals(6, Calcs.sum(1, 5));
     }
 
     @Then("^I should be logged in$")
     public void iShouldBeLoggedIn() throws Throwable {
-        String result = new String("hello");
-        assertEquals(result.length(), 0);
+//        String result = new String("hello");
+//        assertEquals(result.length(), 0);
     }
 
     @Given("^the following users exist:$")
     public void theFollowingUsersExist() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
 
     }
 
     @But("^Clicking the Create New requirement$")
     public void clickingTheCreateNewRequirement() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
 
     }
 
     @And("^Clicking the Create New defect$")
     public void clickingTheCreateNewDefect() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
 
     }
 
     @When("^Clicking the New Feature$")
     public void clickingTheNewFeature() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
 
     }
 
     @Then("^I should receive an email with:$")
     public void iShouldReceiveAnEmailWith() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
 
     }
 
     @Given("^(\\d+) apples exist$")
     public void applesExist(int arg0) throws Throwable {
-        assertEquals(6, Calcs.sum(2, 5));
+//        assertEquals(6, Calcs.sum(2, 5));
 
     }
 
     @When("^(\\d+) apple is eaten$")
     public void appleIsEaten(int arg0) throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
 
     }
 
     @Then("^(\\d+) apples remain$")
     public void applesRemain(int arg0) throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
 
     }
 }
