25cf6ffac81f42d910539576c9d9bc1d44fc417c
alexandru-dan.bratfalean <alexandru-dan.bratfalean@microfocus.com>
2019-01-28 11:56:26 +0200

    BackgroundStepFail

--- src/test/java/MyStepdefs.java
+++ src/test/java/MyStepdefs.java
@@ -19,7 +19,7 @@ public class MyStepdefs {
 
     @Then("^Two apples remain$")
     public void twoApplesRemain() throws Throwable {
-        assertEquals(6, Calcs.sum(2, 5));
+//        assertEquals(6, Calcs.sum(2, 5));
 
     }
 
@@ -30,58 +30,16 @@ public class MyStepdefs {
 
     @When("^I submit username and password$")
     public void iSubmitUsernameAndPassword() throws Throwable {
-        assertEquals(6, Calcs.sum(1, 6));
 
     }
 
     @Then("^I should be logged in$")
     public void iShouldBeLoggedIn() throws Throwable {
+        String result = new String("hello");
+        assertEquals(result.length(), 0);
 
     }
-
-    @Given("^the following users exist$")
-    public void theFollowingUsersExist() throws Throwable {
-
-    }
-
-    @But("^Clicking the Create New requirement$")
-    public void clickingTheCreateNewRequirement() throws Throwable {
-
-    }
-
-    @And("^Clicking the Create New defect$")
-    public void clickingTheCreateNewDefect() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        assertEquals(6, Calcs.sum(2, 5));
-
-    }
-
-    @When("^Clicking the New Feature$")
-    public void clickingTheNewFeature() throws Throwable {
-
-    }
-
-    @Then("^I should receive an email with$")
-    public void iShouldReceiveAnEmailWith() throws Throwable {
-
-    }
-
-    @Given("^I'm in tablets search results$")
-    public void iMInTabletsSearchResults() throws Throwable {
-
-    }
-
-    @When("^filter by price lower than one thousand$")
-    public void filterByPriceLowerThanOneThousand() throws Throwable {
-
-    }
-
-    @Then("^Only tablets with lower price will be presented$")
-    public void onlyTabletsWithLowerPriceWillBePresented() throws Throwable {
-
-    }
-
- }
+}
 
 //        String result = new String("hello");
 //        assertEquals(result.length(), 0);
