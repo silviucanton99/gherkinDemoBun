91acf73aa245207c91286078b15faf64817a014d
alexandru-dan.bratfalean <alexandru-dan.bratfalean@microfocus.com>
2019-05-08 13:59:31 +0300

    Latest Cucumber and deleted the GT from IDE

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -3,7 +3,7 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin="com.hpe.alm.octane.OctaneGherkinFormatter:gherkin-results\\ManualRunnerTest_OctaneGherkinResults.xml",
+@CucumberOptions(plugin="com.hpe.alm.octane.OctaneGherkinFormatter:gherkin-results\\\\ManualRunnerTest_OctaneGherkinResults.xml",
         features="src/test/resources/features/manualRunner")
 public class MyTest {
 }
