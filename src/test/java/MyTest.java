e7299ca173e3feeb24ce02c8da2d7eb728e4b870
alexandru-dan.bratfalean <alexandru-dan.bratfalean@microfocus.com>
2019-05-14 10:21:52 +0300

    modified pom 2

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -4,6 +4,6 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(plugin="com.hpe.alm.octane.OctaneGherkinFormatter:gherkin-results\\\\ManualRunnerTest_OctaneGherkinResults.xml",
-        features="src/test/resources/features/manualRunner")
+        features="src/test/resources/features/")
 public class MyTest {
 }
