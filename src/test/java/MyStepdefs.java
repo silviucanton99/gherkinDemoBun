c9a0559975e6fe25bfb8458e56dbb18a5f6f194f
alexandru-dan.bratfalean <alexandru-dan.bratfalean@microfocus.com>
2019-08-14 13:16:03 +0300

    withYaniv2

--- src/test/java/MyStepdefs.java
+++ src/test/java/MyStepdefs.java
@@ -5,58 +5,6 @@ import static org.junit.Assert.assertEquals;
 
 
 public class MyStepdefs {
-
-    @Given("^there is a GT$")
-    public void thereIsAGT() throws Throwable {
-//        assertEquals(77, Calcs.sum(5, 5));
-    }
-
-    @When("^the user modifies the script from IDE and runs the Pipeline$")
-    public void theUserModifiesTheScriptFromIDEAndRunsThePipeline() throws Throwable {
-    }
-
-    @Then("^the buttons should appear in Octane$")
-    public void theButtonsShouldAppearInOctane() throws Throwable {
-    }
-
-    @Given("^the following users exist$")
-    public void theFollowingUsersExist() throws Throwable {
-//        assertEquals(199, Calcs.sum(2, 5));
-    }
-
-    @But("^Clicking the Create New requirement$")
-    public void clickingTheCreateNewRequirement() throws Throwable {
-    }
-
-    @And("^Clicking the Create New defect$")
-    public void clickingTheCreateNewDefect() throws Throwable {
-    }
-
-    @When("^Clicking the New Feature$")
-    public void clickingTheNewFeature() throws Throwable {
-    }
-
-    @Then("^I should receive an email with$")
-    public void iShouldReceiveAnEmailWith() throws Throwable {
-//        assertEquals(111, Calcs.sum(100, 11));
-    }
-
-
-    @Given("^I'm in tablets search results$")
-    public void iMInTabletsSearchResults() throws Throwable {
-    }
-
-    @When("^filter by price lower than one thousand$")
-    public void filterByPriceLowerThanOneThousand() throws Throwable {
-        String result = new String("hello");
-//        assertEquals(result.length(), 555);
-    }
-
-    @Then("^Only tablets with lower price will be presented$")
-    public void onlyTabletsWithLowerPriceWillBePresented() throws Throwable {
-    }
-
-
     @Given("Background given")
     public void backgroundGiven() {
 
@@ -90,6 +38,7 @@ public class MyStepdefs {
 
     @Given("b and 2 are known")
     public void band2areknown() {
+        assertEquals(77, Calcs.sum(5, 5));
     }
 
     @When("the credentials are written")
@@ -98,11 +47,73 @@ public class MyStepdefs {
     }
 
     @And("the {string} button is clicked")
-    public void theButtonIsClicked(String arg0) {
+    public void theButtonIsClicked() {
 
     }
 
     @Then("The login is successful")
     public void theLoginIsSuccessful() {
     }
+
+    @Given("there is a GT")
+    public void thereIsAGT() {
+
+    }
+
+    @When("the user modifies the script from IDE and runs the Pipeline")
+    public void theUserModifiesTheScriptFromIDEAndRunsThePipeline() {
+
+    }
+
+    @Then("the buttons should appear in Octane")
+    public void theButtonsShouldAppearInOctane() {
+        String result = new String("hello");
+        assertEquals(result.length(), 555);
+
+    }
+
+    @Given("the following users exist")
+    public void theFollowingUsersExist() {
+
+    }
+
+    @But("Clicking the Create New requirement")
+    public void clickingTheCreateNewRequirement() {
+
+    }
+
+    @And("Clicking the Create New defect")
+    public void clickingTheCreateNewDefect() {
+
+    }
+
+    @When("Clicking the New Feature")
+    public void clickingTheNewFeature() {
+
+    }
+
+    @Then("I should receive an email with")
+    public void iShouldReceiveAnEmailWith() {
+
+    }
+
+    @Given("I'm in tablets search results")
+    public void iMInTabletsSearchResults() {
+
+    }
+
+    @When("filter by price lower than one thousand")
+    public void filterByPriceLowerThanOneThousand() {
+
+    }
+
+    @Then("Only tablets with lower price will be presented")
+    public void onlyTabletsWithLowerPriceWillBePresented() {
+    }
 }
+
+//
+
+//    String result = new String("hello");
+//        assertEquals(result.length(), 555);
+
