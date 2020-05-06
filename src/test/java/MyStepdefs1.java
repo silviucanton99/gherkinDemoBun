8a80509a52f8de6bb8d74cb9232222b6b916db1e
alexandru-dan.bratfalean <alexandru-dan.bratfalean@microfocus.com>
2020-05-06 09:36:25 +0300

    1k?

---
+++ src/test/java/MyStepdefs1.java
@@ -0,0 +1,22 @@
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+
+public class MyStepdefs1 {
+
+    @Given("^aaa$")
+    public void aaa() {
+    }
+
+    @Given("^bbb")
+    public void bbb() {
+    }
+
+    @Given("^ccc")
+    public void ccc() {
+    }
+
+    @Then("^Scenario:$")
+    public void scenario() {
+    }
+}
+
