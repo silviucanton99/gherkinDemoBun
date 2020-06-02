b5de8b3017f6eae8f1a76aa2b6584cab4bf96165
alexandru-dan.bratfalean <alexandru-dan.bratfalean@microfocus.com>
2020-06-02 17:31:34 +0300

    2fail

--- src/test/java/MyStepdefs.java
+++ src/test/java/MyStepdefs.java
@@ -19,7 +19,7 @@ public class MyStepdefs {
     @Then("the buttons should appear in Octane")
     public void theButtonsShouldAppearInOctane() {
         String result = "hello";
-        assertEquals(result.length(), 5);
+        assertEquals(result.length(), 50);
 
     }
 
@@ -312,7 +312,7 @@ public class MyStepdefs {
     @Then("the LAPTOP is modified")
     public void theLAPTOPIsModified() {
         String result = "laptop";
-        assertEquals(result.length(), 6);
+        assertEquals(result.length(), 60);
 
     }
 
