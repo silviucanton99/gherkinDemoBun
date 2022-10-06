9b07dfb073295a916e60d6a1714f1237cd5ca4e8
Silviu-Constantin Anton <84314479+silviucanton99@users.noreply.github.com>
2022-10-06 16:13:51 +0300

    Update demo.java

--- src/main/java/demo.java
+++ src/main/java/demo.java
@@ -3,7 +3,7 @@ public class demo {
     //This is the main function
     //Second comment
     //This fixes the defect
-    public static void main(String [ ] args) {
+    public static void main(String [] args) {
         System.out.println("Gherkin Demo");
     }
 
