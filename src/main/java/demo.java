1868cc8c244dba0dc6ac0efd81fe593206eee60d
Silviu-Constantin Anton <84314479+silviucanton99@users.noreply.github.com>
2022-10-06 12:11:40 +0300

    Defect#1002

--- src/main/java/demo.java
+++ src/main/java/demo.java
@@ -2,6 +2,7 @@ public class demo {
 
     //This is the main function
     //Second comment
+    //This fixes the defect
     public static void main(String [ ] args) {
         System.out.println("Gherkin Demo");
     }
